388922df66cb1d2488549b995e9e1d31fadf5e39
igi33 <stosicigor96@gmail.com>
2019-09-17 11:18:15 +0200

    Added DB scripts and tweaked the readme file

--- README.md
+++ README.md
@@ -12,19 +12,31 @@ A simple online judge platform which grades program solutions automatically.
 
 ## Frameworks and Requirements
 #### Back End: ASP.NET Core - C# Web API Framework by Microsoft
-ASP.NET Core Web API version used: 2.2
+ASP.NET Core Web API version used: 2.2\
 Authentication handled via JSON Web Tokens (JWT)
 
 Requirements:
 
-* .NET Core 2.2 SDK
-* MySQL 8.0.11
+- .NET Core 2.2 SDK
+- MySQL 8.0.11
 
 #### Front End: WPF - C# Front End Framework for Windows by Microsoft
-...
+.NET Framework version used: 4.7.2
 
-## Serving the Application
-...
+## Running the Application
+#### Database
+- Run the two SQL scripts from the **/database-scripts/ directory** to create the project MySQL database schema and to populate it with initial data
+- The project back end assumes the following MySQL connection pairs: localhost for the server, root for the username and root for the password. Change this if needed in the **/OnlineJudgeApi/OnlineJudgeApi/appsettings.json file**
+- The given table data row for C++ compilation in the ComputerLanguages table assumes the following location of the compiler: **C:/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64/bin**. Change this entry to where your compiler is located!
+
+#### Back End
+To serve the API locally run the following command from the **/OnlineJudgeApi/OnlineJudgeApi/ directory**:
+```console
+dotnet run
+```
+#### Front End
+1. Build the OnlineJudgeWpfApp project
+2. Run the project from Visual Studio, or find the executable in the **/OnlineJudgeWpfApp/OnlineJudgeWpfApp/bin/Debug/ directory**
 
 ## Useful Links
 * [JSON Web Tokens](https://jwt.io/) - Authorization tokens for a RESTful API
---
+++ database-scripts/db_schema_online_judge.sql
@@ -0,0 +1,260 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Sep 16, 2019 at 04:36 PM
+-- Server version: 8.0.11
+-- PHP Version: 7.2.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `online_judge`
+--
+CREATE DATABASE IF NOT EXISTS `online_judge` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `online_judge`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `computerlanguages`
+--
+
+CREATE TABLE `computerlanguages` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
+  `extension` varchar(16) COLLATE utf8mb4_general_ci NOT NULL,
+  `compilerpath` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `compilerfilename` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
+  `compilecmd` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `executecmd` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `submissions`
+--
+
+CREATE TABLE `submissions` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `taskid` int(10) UNSIGNED NOT NULL,
+  `langid` int(10) UNSIGNED NOT NULL,
+  `userid` int(10) UNSIGNED NOT NULL,
+  `timesubmitted` datetime NOT NULL,
+  `sourcecode` text COLLATE utf8mb4_general_ci NOT NULL,
+  `status` varchar(16) COLLATE utf8mb4_general_ci NOT NULL,
+  `executiontime` int(10) UNSIGNED NOT NULL,
+  `executionmemory` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+CREATE TABLE `tags` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
+  `description` text COLLATE utf8mb4_general_ci NOT NULL,
+  `memorylimit` int(10) UNSIGNED NOT NULL,
+  `timelimit` int(10) UNSIGNED NOT NULL,
+  `userid` int(10) UNSIGNED NOT NULL,
+  `timesubmitted` datetime NOT NULL,
+  `origin` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasktags`
+--
+
+CREATE TABLE `tasktags` (
+  `taskid` int(10) UNSIGNED NOT NULL,
+  `tagid` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `testcases`
+--
+
+CREATE TABLE `testcases` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `taskid` int(10) UNSIGNED NOT NULL,
+  `input` text COLLATE utf8mb4_general_ci NOT NULL,
+  `output` text COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `username` varchar(64) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(128) COLLATE utf8mb4_general_ci NOT NULL,
+  `timeregistered` datetime NOT NULL,
+  `passwordhash` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `passwordsalt` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `computerlanguages`
+--
+ALTER TABLE `computerlanguages`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `submissions`
+--
+ALTER TABLE `submissions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `taskid` (`taskid`),
+  ADD KEY `langid` (`langid`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indexes for table `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indexes for table `tasktags`
+--
+ALTER TABLE `tasktags`
+  ADD PRIMARY KEY (`taskid`,`tagid`),
+  ADD KEY `tasktags_ibfk_1` (`tagid`);
+
+--
+-- Indexes for table `testcases`
+--
+ALTER TABLE `testcases`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `taskid` (`taskid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `computerlanguages`
+--
+ALTER TABLE `computerlanguages`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `submissions`
+--
+ALTER TABLE `submissions`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `testcases`
+--
+ALTER TABLE `testcases`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `submissions`
+--
+ALTER TABLE `submissions`
+  ADD CONSTRAINT `submissions_ibfk_1` FOREIGN KEY (`taskid`) REFERENCES `tasks` (`id`),
+  ADD CONSTRAINT `submissions_ibfk_2` FOREIGN KEY (`userid`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `submissions_ibfk_3` FOREIGN KEY (`langid`) REFERENCES `computerlanguages` (`id`);
+
+--
+-- Constraints for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `tasktags`
+--
+ALTER TABLE `tasktags`
+  ADD CONSTRAINT `tasktags_ibfk_1` FOREIGN KEY (`tagid`) REFERENCES `tags` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tasktags_ibfk_2` FOREIGN KEY (`taskid`) REFERENCES `tasks` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `testcases`
+--
+ALTER TABLE `testcases`
+  ADD CONSTRAINT `testcases_ibfk_1` FOREIGN KEY (`taskid`) REFERENCES `tasks` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
